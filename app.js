15b93deeb160c201c476e776730ea625f1e04bb4
Deepesh Makhijani <deepeshmak@gmail.com>
2017-03-17 22:43:34 +0530

    Basic Backend Setup

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var home = require('./routes/home');
-var indivBlog = require('./routes/users');
+var about = require('./routes/about');
+var blog = require('./routes/blog');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -23,7 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', home);
-app.use('/users', indivBlog);
+app.use('/about', about);
+app.use('/blog', blog);
+app.use('/contact', contact);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
