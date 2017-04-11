f14c9b6fb716f3060babc41dc7b5ac927b07ac4b
Deepesh Makhijani <deepeshmak@gmail.com>
2017-04-11 10:41:17 +0530

    Deployment Related

--- app.js
+++ app.js
@@ -24,7 +24,18 @@ var io = socket_io();
 app.io = io;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://user:OriOn21*@mongodb/sampledb')
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/sampledb';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect('mongodb://'+ connection_string);
 mongoose.Promise = global.Promise;
 
 // view engine setup
